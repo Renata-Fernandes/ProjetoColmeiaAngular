f58c935b83f0ce1c8925ea959c972a09665edcd7
Bárbara Lopes <barbara.lopes@escriba.com.br>
2022-08-13 01:06:46 -0300

    inicio das rotas - incompleto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginDoadorComponent } from './login-doador/login-doador.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login-doador', component:LoginDoadorComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/colmeia/colmeia-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+//import { NgChartsModule } from 'ng2-charts';
+import { ClienteLogin } from '../model/ClienteLogin';
+import { ColmeiaComponent } from './colmeia.component';
+import { HomeComponent } from './home/home.component';
+import { ProdutosBeneficiarioComponent } from './produtos-beneficiario/produtos-beneficiario.component';
+import { CadastroBeneficiarioComponent } from './cadastro-beneficiario/cadastro-beneficiario.component';
+import { SobreComponent } from '../sobre/sobre.component';
+import { ProdutosBeneficiarioModule } from '../produtos-beneficiario/produtos-beneficiario.module';
+
+const routes: Routes = [
+    {
+        //aqui vai tudo que fica fixo ao menu e que precisa de rota ou que vai ser usado dentro dessa parte
+        path:'', component: ColmeiaComponent, children:[
+            {path:'', redirectTo:'home', pathMatch:'full'},
+            {path:'home', component: HomeComponent},
+            {path:'produtos-beneficiario', loadChildren:()=>ProdutosBeneficiarioModule,
+            {path:'cadastro-beneficiario', component: CadastroBeneficiarioComponent},
+            {path:'sobre', component: SobreComponent},
+
+        ]
+
+    }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class ColmeiaModule { }
